58c764413e9d697016a8c11b488e09b9b15f1afb
LilOnion8989 <thomasnel878@gmail.com>
2026-05-28 22:48:33 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+include 'db_connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM members 
+            WHERE username = '$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['username'] = $username;
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Wrong password.";
+        }
+    } else {
+        echo "User not found.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Login</title>
+</head>
+
+<body>
+    <h1>Login</h1>
+    <form method="POST">
+
+        <label>Username</label><br>
+        <input type="text" name="username" required><br><br>
+
+        <label>Password</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+
+</body>
+</html>
